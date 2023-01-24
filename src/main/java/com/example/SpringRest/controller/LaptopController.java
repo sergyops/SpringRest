53f2431dd978d12fa11b9924fc51c0f67b02d4ff
sergyops <70977028+sergyops@users.noreply.github.com>
2023-01-24 16:39:05 +0100

    Added CRUD methods: Find, Create, Update, Delete CRUD methods and added postman collection

--- src/main/java/com/example/SpringRest/controller/LaptopController.java
+++ src/main/java/com/example/SpringRest/controller/LaptopController.java
@@ -2,15 +2,22 @@ package com.example.SpringRest.controller;
 
 import com.example.SpringRest.entities.Laptop;
 import com.example.SpringRest.repository.LaptopRepository;
+import org.apache.coyote.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class LaptopController {
 
-    LaptopRepository laptopRepository;
+    private final Logger log = LoggerFactory.getLogger(LaptopController.class);
+
+    private LaptopRepository laptopRepository;
 
     public LaptopController(LaptopRepository laptopRepository) {
         this.laptopRepository = laptopRepository;
@@ -23,10 +30,59 @@ public class LaptopController {
         return laptopRepository.findAll();
     }
 
-    @PostMapping("/api/laptops")
-    public Laptop create(@RequestBody Laptop book, @RequestHeader HttpHeaders headers){
-        System.out.println(headers.get("User-Agent"));//see who is sending information
-        return laptopRepository.save(book);
+    @GetMapping("/api/laptops/{id}")
+    public ResponseEntity<Laptop> findOneById(@PathVariable Long id){
 
+        Optional<Laptop> laptopOpt = laptopRepository.findById(id);
+
+        if(laptopOpt.isPresent()){
+            return ResponseEntity.ok(laptopOpt.get());//if laptop exist return a 200 code
+        }else {
+            return ResponseEntity.notFound().build();//if laptop doesn't exist return a 404 not found code
+        }
+    }
+
+    @PostMapping("/api/laptops")
+    public ResponseEntity<Laptop> create(@RequestBody Laptop laptop, @RequestHeader HttpHeaders headers){
+        System.out.println(headers.get("User-Agent"));//see who is sending information
+        if (laptop.getId() != null) {//
+            log.warn("trying to create a laptop with id");
+            return ResponseEntity.badRequest().build();
+        }
+        Laptop result = laptopRepository.save(laptop);
+        return ResponseEntity.ok(result);
+
+    }
+
+    @PutMapping("/api/laptops")
+    public ResponseEntity<Laptop> update(@RequestBody Laptop laptop){
+        if (laptop.getId()==null){//if doesn't has a id
+            log.warn("Trying to update a non existent laptop");
+            return ResponseEntity.badRequest().build();
+        }
+        if (!laptopRepository.existsById(laptop.getId())){//if doesn't exist with this id
+            log.warn("Trying to update a non existent laptop");
+            return ResponseEntity.notFound().build();
+        }
+        Laptop result = laptopRepository.save(laptop);
+        return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping("/api/laptops/{id}")
+    public ResponseEntity<Laptop> delete(@PathVariable Long id){
+
+        if (!laptopRepository.existsById(id)){
+            log.warn("trying to delete a non existent laptop");
+            return ResponseEntity.notFound().build();
+        }
+        laptopRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/api/laptops")
+    public ResponseEntity<Laptop> deleteAll(){
+        log.info("REST request for delete all books");
+        laptopRepository.deleteAll();
+        return ResponseEntity.noContent().build();
     }
 }
